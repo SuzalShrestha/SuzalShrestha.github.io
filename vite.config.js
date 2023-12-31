7286e22084921acf9c4fdde2f2a2baf61b233c6e
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-12-31 13:49:16 +0545

    completed deployement yml

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  //github page deployment
+  base: "/adoptme",
   plugins: [react()],
   root: "src",
 });
